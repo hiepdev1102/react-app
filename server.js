b9f3129cf5c138345e7f4127eb0be8cb1c46de4e
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2020-02-13 00:37:21 +0700

    13/02 progress

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 //deprecated since ver 4.16, now intergrated in express:
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
 //deprecated:
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
+// Connect database:
+connectDB();
 
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose
+  .connect(
+    process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`
+  )
+  .then();
 
 app.use(express.json());
 
+app.use('/api/users', require('./routes/api/users'));
+/*
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/posts'));
+*/
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
